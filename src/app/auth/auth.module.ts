5d84e5c0fa8232730bced899153784a5cd332510
iamdeepz <deepakchandrasekaran14@gmail.com>
2022-07-10 16:24:42 +0530

    auth signup page completed

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,20 +3,23 @@ import { CommonModule } from '@angular/common';
 import { AuthComponent } from './auth.component';
 import { LoginComponent } from './login/login.component';
 import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
+    {path: '', redirectTo: 'account', pathMatch: 'full'},
     {
-        path: '', 
+        path: 'account', 
         component: AuthComponent, 
         children: [
             {path: 'login', component: LoginComponent},
+            {path: 'signup', component: SignupComponent},
             {path: '', redirectTo: 'login', pathMatch: 'full'}
         ]
     }
 ]
 
 @NgModule({
-    declarations: [AuthComponent, LoginComponent],
+    declarations: [AuthComponent, LoginComponent, SignupComponent],
     imports: [
         CommonModule,
         RouterModule.forChild(routes)
