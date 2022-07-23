0fbd1261c7d8a40934fcd4f2c56de780462bbf6a
iamdeepz <deepakchandrasekaran14@gmail.com>
2022-07-23 20:07:56 +0530

    footer updated

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+declare const $: any;
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
-  styleUrls: ['./pages.component.css']
+  styleUrls: ['./pages.component.scss']
 })
 export class PagesComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+    // $('#sidebar-toggle').click(function(){
+    //   console.log('clicked');
+    //   $("#wrapper").toggleClass("toggled");
+    // })
   }
 
 }
